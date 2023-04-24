12eadeac32daa2051cdfd8db6f78ec6dddbc2f44
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-25 01:40:34 +0300

    lab comp

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,15 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        list = []
+        people = json.loads(self.get_response_body())
+        for person in people:
+            list.append(person)
+
+        return list
